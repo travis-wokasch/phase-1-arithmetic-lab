dd9d115a004a3ce8ac9454cb89af7ed896c238ba
Travis <tjwokasch@gmail.com>
2021-07-06 14:53:20 -0600

    commit tree 08ef175689ae28117c033e8775be7d6ce64b7230

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2
+
+let random = Math.floor(Math.random() *10)
+console.log(random)
+
+let num3 = 16
+let num4 = 4
+const mod = num3 / num4
+
+const max = Math.max(1,2,3,4,20)
